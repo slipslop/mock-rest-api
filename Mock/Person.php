6a67e9f237bb5c49177a6218028c56c8ddf6720b
Vili Taka-Anttila <vili.takaanttila@gmail.com>
2021-05-24 19:00:06 +0300

    initial commit

---
+++ Mock/Person.php
@@ -0,0 +1,64 @@
+<?php 
+
+class Mock_Person{
+    
+    private $requiredFields = ['name', 'email'];
+
+    function __construct( array $attr ){
+        $this->id = uniqid();
+        $this->name = $attr['name'];
+        $this->email = $attr['email'];
+        $this->created = date('Y-m-d H:i:s');
+    }
+
+    public function getName() : ?string {
+        return $this->name;
+    }
+
+    public function setName( string $name ){
+        $this->name = $name;
+    }
+
+    public function getEmail() : ?string {
+        return $this->email;
+    }
+
+    public function setEmail( string $email ){
+        $this->email = $email;
+    }
+
+    public function getCreated() : DateTime {
+        return new Datetime( $this->created );
+    }
+
+    public function setCreated( $date ){
+        $this->created = $date;
+    }
+
+    public function validate(){
+
+        foreach( $this->requiredFields as $field ) {
+            
+            $fieldUppercase = ucfirst($field);
+
+            $method = "get{$fieldUppercase}";
+           
+            if( !method_exists($this, $method) ) {
+                $this->error = "No getter for {$method} ";
+                return true;
+            }
+           
+            if( !$this->$method() ) {
+                $this->error = $field;
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+}
+
+?>
---
+++ api/person/read.php
@@ -0,0 +1,15 @@
+<?php
+http_response_code(200);
+
+$date = new DateTime();
+
+$mockup = [
+    'id'        => uniqid(),
+    'name'      => 'Foo Bar',
+    'email'     => 'foo@bar.com',
+    'created'   => $date->format('Y-m-d H:i:s'),
+];
+
+echo( json_encode( $mockup ) );
+
+?>
